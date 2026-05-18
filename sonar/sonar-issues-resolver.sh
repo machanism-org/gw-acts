10be52d4f1ff0a2c55a34c6f3bc8847abb2e11a1
ViktorTovstyi <viktor.tovstyi@gmail.com>
2026-05-18 10:02:52 +0300

    updated version.

--- sonar/sonar-issues-resolver.sh
+++ sonar/sonar-issues-resolver.sh
@@ -6,7 +6,7 @@ export GENAI_PASSWORD="..."
 export SONAR_HOST_URL="https://..."
 export SONAR_TOKEN="squ_..."
 
-mvn org.machanism.machai:gw-maven-plugin:1.1.12-SNAPSHOT:act-per-module \
+mvn org.machanism.machai:gw-maven-plugin:1.1.12:act-per-module \
   -Dgw.act="C:\projects\gw-acts\sonar\sonar-issues-resolver.toml Set SOURCE_BRANCH: 'main'. Set QUALITIES: 'SECURITY'. Set SEVERITY: 'MEDIUM'. Use '-s C:\Users\ViktorTovstyi\.m2\settings.xml' for every maven call. Use MR_TITLE format: '[AI][[QUALITIES]][SONAR] <title>'. Use the '[AI][[QUALITIES]][SONAR]' prefix for every commit message." \
 
   
